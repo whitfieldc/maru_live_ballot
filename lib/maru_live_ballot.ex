9129b0e3c58eda22b38f4e5cc3ec8640bbbff9a3
Charles <whitfield.cw@gmail.com>
2015-12-02 19:57:14 -0800

    add initial hello world json

--- lib/maru_live_ballot.ex
+++ lib/maru_live_ballot.ex
@@ -1,2 +1,13 @@
-defmodule MaruLiveBallot do
+defmodule MaruLiveBallot.API do
+  use Maru.Router
+
+  get do
+    json(conn, %{ hello: :world })
+  end
+
+  rescue_from :all, as: e do
+    IO.inspect(e)
+    put_status(conn, 500)
+    text(conn, "Server Error")
+  end
 end
