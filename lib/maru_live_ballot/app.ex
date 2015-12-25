511e42790a1110c5049afc94c0c5211255ea0d10
Charles Whitfield <whitfield.cw@gmail.com>
2015-12-24 22:59:43 -0600

    add post route to query wrapper

--- lib/maru_live_ballot/app.ex
+++ lib/maru_live_ballot/app.ex
@@ -29,6 +29,13 @@ defmodule MaruLiveBallot.QueryWrapper do
       |> Database.run
   end
 
+  def create_ballot(new_ballot) do
+    post = table("posts")
+      |> insert(new_ballot)
+      # |> IO.inspect
+      |> Database.run
+  end
+
   def get_ballot_by_id(id_to_get) do
     ballot = table("posts")
       |> filter(%{id: id_to_get})
@@ -38,10 +45,10 @@ defmodule MaruLiveBallot.QueryWrapper do
 
   def update_tally(id, choice) do
     ballot = table("posts")
-          |> get(id)
-          |> update(lambda fn (doc) -> %{tallies: %{grizzly_bear: doc["tallies"]["grizzly_bear"] +1}} end)
-          |> Database.run
-          |> IO.inspect
+      |> get(id)
+      |> update(lambda fn (doc) -> %{tallies: %{grizzly_bear: doc["tallies"]["grizzly_bear"] +1}} end)
+      |> Database.run
+      |> IO.inspect
   end
 end
 
@@ -49,7 +56,7 @@ defmodule MaruLiveBallot.Router.Endpoint do
   use Maru.Router
   alias MaruLiveBallot.Database
 
-  import RethinkDB.Query, only: [table_create: 1, table: 2, table: 1, insert: 2, filter: 2]
+  # import RethinkDB.Query, only: [table_create: 1, table: 2, table: 1, insert: 2, filter: 2]
 
 
   namespace :ballots do
@@ -81,10 +88,7 @@ defmodule MaruLiveBallot.Router.Endpoint do
 
       formatted_ballot = Map.put(params_ballot, :tallies, tallies)
 
-      post = table("posts")
-        |> insert(formatted_ballot)
-        |> IO.inspect
-        |> Database.run
+      post = MaruLiveBallot.QueryWrapper.create_ballot(formatted_ballot)
       json(conn, post.data)
     end
 
@@ -106,9 +110,8 @@ defmodule MaruLiveBallot.Router.Endpoint do
       end
 
       patch "/tallies" do
-        # curl -H "Content-Type: application/json" -X PATCH -d '{"vote": "grizzly_bear"}' http://localhost:8880/ballots/e5632783-d472-48af-8e82-f271bceb4f8d/tallies | less
+        # curl -H "Content-Type: application/json" -X PATCH -d '{"vote": "grizzly_bear"}' http://localhost:8880/ballots/40226528-f4c3-4b33-9c78-5aa04001b2eb/tallies | less
         vote = params[:vote] |> IO.inspect
-
 
         MaruLiveBallot.QueryWrapper.update_tally(params[:id], vote) |> IO.inspect
 
